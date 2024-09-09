d053fb94a9bba68a67b311c222562c9bcf1dff46
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-09-09 06:51:54 +0100

    Improve interface for error expanation

--- R/parse_error.R
+++ R/parse_error.R
@@ -69,31 +69,75 @@ cnd_footer.odin_parse_error <- function(cnd, ...) {
 ##' @param code The error code, as a string, in the form `Exxxx` (a
 ##'   capital "E" followed by four numbers)
 ##'
+##' @param how How to explain the error. Options are `pretty` (render
+##'   pretty text in the console), `plain` (display plain text in the
+##'   console) and `link` (browse to the online help).
+##'
 ##' @return Nothing, this is called for its side effect only
 ##'
 ##' @export
-odin_error_explain <- function(code) {
-  ## There are a couple of options here that we can explore:
-
-  ## * We might write out html and read that in as html
-  ## * We might render the markdown using cli
-  ## * We might page the file with page()
-  ## * We might send the user to the web page version
-
+odin_error_explain <- function(code, how = "pretty") {
   assert_scalar_character(code)
   if (!grepl("^E[0-9]{4}$", code)) {
     cli::cli_abort("Invalid code '{code}', should match 'Exxxx'",
                    arg = "code")
   }
-  txt <- errors[[code]]
-  if (is.null(txt)) {
+  err <- errors[[code]]
+  if (is.null(err)) {
     cli::cli_abort(
       c("Error '{code}' is undocumented",
         i = paste("If you were directed here from an error message, please",
                   "let us know (e.g., file an issue or send us a message)")),
       arg = "code")
   }
-  url <- sprintf("https://mrc-ide.github.io/odin2/articles/errors.html#%s",
-                 tolower(code))
-  utils::browseURL(url)
+
+  how <- match_value(how, c("pretty", "plain", "link"))
+  if (how == "link") {
+    url <- sprintf("https://mrc-ide.github.io/odin2/articles/errors.html#%s",
+                   tolower(code))
+    utils::browseURL(url)
+  } else {
+    render_error(err, how == "pretty")
+  }
+}
+
+
+## This is probably destined for the utils package once we're happy
+## with it.
+render_error <- function(err, pretty) {
+  render_paragraph <- function(el) {
+    cli::cli_par()
+    cli::cli_text(el$text)
+  }
+
+  render_code_block <- function(el) {
+    cli::cli_par()
+    cli::cli_code(paste0("    ", el$text))
+  }
+
+  render_list <- function(el) {
+    cli::cli_par()
+    if (el$mode == "bullet") {
+      cli::cli_ul()
+    } else {
+      cli::cli_ol()
+    }
+    for (i in el$items) {
+      cli::cli_li(i$text)
+    }
+  }
+
+  if (pretty) {
+    cli::cli_h1(err$code)
+    for (el in err$parsed) {
+      switch(
+        el$type,
+        paragraph = render_paragraph(el),
+        code_block = render_code_block(el),
+        list = render_list(el),
+        cli::cli_abort("Unknown error block type '{el$type}' while rendering"))
+    }
+  } else {
+    writeLines(c(sprintf("# %s", err$code), "", err$plain))
+  }
 }
