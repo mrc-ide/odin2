75c7fb48efcb44710151bfc26e78d46ef676dc5d
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-11-13 15:52:24 +0000

    Test dependencies joining

--- tests/testthat/test-dependencies.R
+++ tests/testthat/test-dependencies.R
@@ -23,3 +23,21 @@ test_that("can report back on cyclic dependencies", {
   expect_equal(topological_order(deps),
                list(success = FALSE, error = 3:5))
 })
+
+
+test_that("can join dependencies", {
+  deps <- list(
+    list(functions = c("a", "f", "g"), variables = c("a", "b", "c")),
+    list(functions = "f",              variables = c("c", "d", "e")),
+    list(functions = c("h", "i"),      variables = character()))
+  expect_equal(
+    join_dependencies(list()),
+    list(functions = character(), variables = character()))
+  expect_equal(
+    join_dependencies(deps[1]),
+    deps[[1]])
+  expect_equal(
+    join_dependencies(deps),
+    list(functions = c("a", "f", "g", "h", "i"),
+         variables = c("a", "b", "c", "d", "e")))
+})
