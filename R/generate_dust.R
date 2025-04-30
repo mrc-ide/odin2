99cbd3d7f62b21cc1386c3e8a4f868772413fc4f
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-04-30 17:25:49 +0100

    Fix export logic

--- R/generate_dust.R
+++ R/generate_dust.R
@@ -922,13 +922,13 @@ generate_dust_browser <- function(dat, phase) {
                                 dat$sexp_data))
   env <- "odin_env"
   body$add(sprintf("auto %s = dust2::r::browser::create();", env))
-  ## TODO: don't drop 'data' if using browser in compare, but think
-  ## about how probabilities might be surfaced.
+
   type <- dat$storage$type
-  location <- dat$storage$type
-  export <- intersect(
-    names(location[location != "data"]),
-    names(type[type %in% c("real_type", "int", "bool")]))
+  export <- intersect(c(dat$phases$build_shared$equations,
+                        dat$phases$update_shared$equations,
+                        dat$phases[[phase]]$equations,
+                        dat$variables),
+                      names(type)[type %in% c("real_type", "int", "bool")])
   for (v in c("time", export)) {
     body$add(generate_dust_browser_to_env(v, dat, env))
   }
