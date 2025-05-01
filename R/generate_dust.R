830dfc1fc154992f06bb33111d45ae2d46577a9a
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-05-01 16:14:59 +0100

    Drop redundant check

--- R/generate_dust.R
+++ R/generate_dust.R
@@ -411,20 +411,18 @@ generate_dust_system_output <- function(dat) {
     body$add(generate_dust_assignment(eq, "state", dat))
   }
 
-  if (length(dat$output) > 0) {
-    packing <- dat$storage$packing$state
-    for (el in dat$output) {
-      src <- generate_dust_sexp(el, dat$sexp_data)
-      offset <- generate_dust_sexp(call("OdinOffset", "state", el),
-                                   dat$sexp_data)
-      is_array <- el %in% dat$storage$arrays$name
-      if (is_array) {
-        body$add(sprintf(
-          "std::copy(%s.begin(), %s.end(), state + %s);",
-          src, src, offset))
-      } else {
-        body$add(sprintf("state[%s] = %s;", offset, src))
-      }
+  packing <- dat$storage$packing$state
+  for (el in dat$output) {
+    src <- generate_dust_sexp(el, dat$sexp_data)
+    offset <- generate_dust_sexp(call("OdinOffset", "state", el),
+                                 dat$sexp_data)
+    is_array <- el %in% dat$storage$arrays$name
+    if (is_array) {
+      body$add(sprintf(
+        "std::copy(%s.begin(), %s.end(), state + %s);",
+        src, src, offset))
+    } else {
+      body$add(sprintf("state[%s] = %s;", offset, src))
     }
   }
 
